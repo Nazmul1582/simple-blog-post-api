3c29867a374ce55b44cf49e4ab55fafbb2d31816
Nazmul1582 <nazmulhasan1582@gmail.com>
2025-10-31 19:02:09 +0600

    implemented the input sanitization using sanitize-html npm

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const Joi = require("joi")
+const sanitizeHtml = require("sanitize-html")
 const router = express.Router()
 const Blog = require("../model/Blog")
 const blogs = []
 
+// GET API
 router.get("/posts/:id", (req, res) => {
   const id = parseInt(req.params.id)
   const blog = blogs.find((b) => b.id === id)
@@ -18,31 +20,41 @@ router.get("/posts/:id", (req, res) => {
   })
 })
 
+// validation object using Joi
 const blogSchema = Joi.object({
-  title: Joi.string().min(5).required(),
+  title: Joi.string().min(3).required(),
   content: Joi.string().min(10).required(),
   author: Joi.string().required(),
 })
 
+// POST API
 router.post("/posts", (req, res) => {
   const { title, content, author } = req.body
 
+  // Validate the requested fields and handle the error.
   const { error } = blogSchema.validate(req.body)
-
   if (error) {
     return res.status(400).json({
       message: error.details[0].message,
     })
   }
 
+  // sanitize user inputs
+  const sanitizeTitle = sanitizeHtml(title)
+  const sanitizeContent = sanitizeHtml(content)
+  const sanitizeAuthor = sanitizeHtml(author)
+
+  // create new blog
   const blog = new Blog({
     id: blogs.length + 1,
-    title,
-    content,
-    author,
+    title: sanitizeTitle,
+    content: sanitizeContent,
+    author: sanitizeAuthor,
   })
+  // store new blog in blogs array.
   blogs.push(blog)
 
+  // send response
   res.status(201).json({
     message: "Post created successfully!",
     blog,
